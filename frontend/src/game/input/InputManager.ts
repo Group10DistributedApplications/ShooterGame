3da9f3ef6708b7caec87915f2fc9e79da87346aa
karlpedro <162471444+karlpedro@users.noreply.github.com>
2026-01-07 14:45:44 +0100

    edge based keyboard input change.

--- frontend/src/game/input/InputManager.ts
+++ frontend/src/game/input/InputManager.ts
@@ -6,42 +6,30 @@ export type Direction = "up" | "down" | "left" | "right" | null;
 export default class InputManager {
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   private playerId: number;
-  private intervals: Map<string, number> = new Map();
 
   constructor(scene: Phaser.Scene, playerId = 1) {
     this.cursors = scene.input.keyboard!.createCursorKeys();
     this.playerId = playerId;
 
-    // send network inputs on keydown / keyup; send repeated messages while held
     const keyboard = scene.input.keyboard!;
 
-    const repeatInterval = 50; // ms
-
-    const startRepeating = (action: string) => {
-      if (this.intervals.has(action)) return;
+    const startAction = (action: string) => {
       net.sendInput(this.playerId, action);
-      const id = window.setInterval(() => net.sendInput(this.playerId, action), repeatInterval);
-      this.intervals.set(action, id as unknown as number);
     };
 
-    const stopRepeating = (action: string) => {
-      const id = this.intervals.get(action);
-      if (id !== undefined) {
-        clearInterval(id);
-        this.intervals.delete(action);
-      }
+    const stopAction = () => {
       net.sendInput(this.playerId, "STOP");
     };
 
-    keyboard.on("keydown-LEFT", () => startRepeating("LEFT"));
-    keyboard.on("keydown-RIGHT", () => startRepeating("RIGHT"));
-    keyboard.on("keydown-UP", () => startRepeating("UP"));
-    keyboard.on("keydown-DOWN", () => startRepeating("DOWN"));
+    keyboard.on("keydown-LEFT", () => startAction("LEFT"));
+    keyboard.on("keydown-RIGHT", () => startAction("RIGHT"));
+    keyboard.on("keydown-UP", () => startAction("UP"));
+    keyboard.on("keydown-DOWN", () => startAction("DOWN"));
 
-    keyboard.on("keyup-LEFT", () => stopRepeating("LEFT"));
-    keyboard.on("keyup-RIGHT", () => stopRepeating("RIGHT"));
-    keyboard.on("keyup-UP", () => stopRepeating("UP"));
-    keyboard.on("keyup-DOWN", () => stopRepeating("DOWN"));
+    keyboard.on("keyup-LEFT", stopAction);
+    keyboard.on("keyup-RIGHT", stopAction);
+    keyboard.on("keyup-UP", stopAction);
+    keyboard.on("keyup-DOWN", stopAction);
 
     keyboard.on("keydown-SPACE", () => net.sendInput(this.playerId, "FIRE"));
   }
