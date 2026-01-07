61315db1d9edcd59c55937c76a6d3c89b1d95c86
karlpedro <162471444+karlpedro@users.noreply.github.com>
2026-01-07 10:19:53 +0100

    simple movement

--- frontend/src/game/GameScene.ts
+++ frontend/src/game/GameScene.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 
 export default class GameScene extends Phaser.Scene {
   private player!: Phaser.GameObjects.Rectangle;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private speed = 200; // pixels per second
 
   constructor() {
     super("game");
@@ -9,9 +11,25 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     this.player = this.add.rectangle(400, 300, 30, 30, 0x00ff00);
+
+    // Create arrow key input
+    this.cursors = this.input.keyboard!.createCursorKeys();
   }
 
   update(_time: number, delta: number) {
-    this.player.x += 0.05 * delta;
+    const move = (this.speed * delta) / 1000;
+
+    if (this.cursors.left.isDown) {
+      this.player.x -= move;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += move;
+    }
+    if (this.cursors.up.isDown) {
+      this.player.y -= move;
+    }
+    if (this.cursors.down.isDown) {
+      this.player.y += move;
+    }
   }
 }
